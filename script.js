c7820425fd5adcbee42c916640ea7d8b3eabc13e
Riccardo Mannino <80473943+RiccardoMannino@users.noreply.github.com>
2022-08-21 20:46:34 +0200

    fix: problema del ricaricameno chart

--- script.js
+++ script.js
@@ -1,105 +1,105 @@
-const getRepo = async () => {
-  const form = document
-    .querySelector(".form")
-    .addEventListener("submit", async (e) => {
-      e.preventDefault();
+const form = document
+  .querySelector(".form")
+  .addEventListener("submit", async (event) => {
+    event.preventDefault();
 
-      const listaCommit = [];
-      const listaIssue = [];
+    let username = document.querySelector("#Ricerca").value;
 
-      const username = document.querySelector(".testo").value;
-      const userUrl = `https://api.github.com/users/${username}/repos`;
-      const response = await fetch(userUrl);
-      const data = await response.json();
+    const userUrl = `https://api.github.com/users/${username}/repos`;
+    const response = await fetch(userUrl);
+    const data = await response.json();
 
-      let nomeRepo = await data.map((repo) => repo.name);
-      let full = await data.map((repo) => repo.full_name);
-      console.log(nomeRepo);
+    let nomeRepo = await data.map((repo) => repo.name);
 
-      const github = document.querySelector(".github");
-      github.href = `https://github.com/${username}`;
-      github.setAttribute("target", "_blank");
+    // SVG con link della repository dell'utente ricercato
 
-      data.forEach(async (repos) => {
-        let commitUrl = await fetch(
-          `https://api.github.com/repos/${repos.full_name}/commits`
-        );
-        let commitResponse = await commitUrl.json();
-        listaCommit.push(commitResponse.length);
-        myChart.update();
-      });
+    const github = document.querySelector(".github");
+    github.href = `https://github.com/${username}`;
+    github.setAttribute("target", "_blank");
 
-      data.forEach(async (repos) => {
-        let issueUrl = await fetch(
-          `https://api.github.com/repos/${repos.full_name}/issues`
-        );
-        let issueResponse = await issueUrl.json();
-        listaIssue.push(issueResponse.length);
-      });
+    //  Lista commit ed issue
+    const listaCommit = [];
+    const listaIssue = [];
 
-      const dati = {
-        labels: nomeRepo,
-        datasets: [
-          {
-            label: "Commit",
-            data: listaCommit,
-            backgroundColor: ["rgba(75, 192, 192, 0.2)"],
-            borderColor: ["rgba(255, 159, 64, 1)"],
-            borderWidth: 1,
-          },
-          {
-            label: "Issues",
-            data: listaIssue,
-            backgroundColor: ["rgb(153, 102, 255)"],
-            borderColor: ["rgba(255, 159, 64, 1)"],
-            borderWidth: 1,
-          },
-        ],
-      };
-
-      let config = {
-        type: "bar",
-        data: dati,
-        options: {
-          plugins: {
-            legend: {
-              labels: {
-                color: "#fff",
-              },
-            },
-            title: {
-              display: true,
-              text: "Numero di commit per repository",
-              font: {
-                size: 16,
-                family: "tahoma",
-                weight: "normal",
-                style: "italic",
-              },
-              color: "#fff",
-            },
-          },
-          scales: {
-            x: {
-              ticks: {
-                color: "#fff",
-              },
-            },
-            y: {
-              ticks: {
-                color: "#fff",
-              },
-              beginAtZero: true,
-            },
-          },
-        },
-      };
-
-      let myChart = new Chart(
-        document.getElementById("myChart").getContext("2d"),
-        config
+    data.forEach(async (repos) => {
+      let commitUrl = await fetch(
+        `https://api.github.com/repos/${repos.full_name}/commits`
       );
+      let commitResponse = await commitUrl.json();
+      listaCommit.push(commitResponse.length);
+
+      let issueUrl = await fetch(
+        `https://api.github.com/repos/${repos.full_name}/issues`
+      );
+      let issueResponse = await issueUrl.json();
+      listaIssue.push(issueResponse.length);
+
+      myChart.update();
     });
+
+    dati.labels = nomeRepo;
+    dati.datasets[0].data = listaCommit;
+    dati.datasets[1].data = listaIssue;
+  });
+
+const dati = {
+  labels: [],
+  datasets: [
+    {
+      label: "Commit",
+      data: [],
+      backgroundColor: ["rgba(75, 192, 192, 0.2)"],
+      borderColor: ["rgba(255, 159, 64, 1)"],
+      borderWidth: 1,
+    },
+    {
+      label: "Issues",
+      data: [],
+      backgroundColor: ["rgb(153, 102, 255)"],
+      borderColor: ["rgba(255, 159, 64, 1)"],
+      borderWidth: 1,
+    },
+  ],
 };
 
-getRepo();
+const config = {
+  type: "bar",
+  data: dati,
+  options: {
+    plugins: {
+      legend: {
+        labels: {
+          color: "#fff",
+        },
+      },
+      title: {
+        display: true,
+        text: "Numero di commit ed issue per repository (Max 30)",
+        font: {
+          size: 16,
+          family: "tahoma",
+          weight: "normal",
+          style: "italic",
+        },
+        color: "#fff",
+      },
+    },
+    scales: {
+      x: {
+        ticks: {
+          color: "#fff",
+        },
+      },
+      y: {
+        ticks: {
+          color: "#fff",
+        },
+        beginAtZero: true,
+      },
+    },
+  },
+};
+const myChart = new Chart(
+  document.getElementById("myChart").getContext("2d"),
+  config
+);
